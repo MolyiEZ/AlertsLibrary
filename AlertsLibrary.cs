e18387356b27d2b3e2ca08e1b293756f861fbf46
Molyi <MolyiEZ@gmail.com>
2024-08-22 18:23:52 -0300

    Initialized service on load.

--- AlertsLibrary.cs
+++ AlertsLibrary.cs
@@ -2,23 +2,26 @@
 using Cysharp.Threading.Tasks;
 using OpenMod.Unturned.Plugins;
 using OpenMod.API.Plugins;
+using Microsoft.Extensions.DependencyInjection;
+using Molyi.Alerts.Interfaces;
 
 // For more, visit https://openmod.github.io/openmod-docs/devdoc/guides/getting-started.html
 
-[assembly: PluginMetadata("Molyi.ZAlertsLibrary", DisplayName = "ZAlertsLibrary")]
+[assembly: PluginMetadata("Molyi.AlertsLibrary", Author = "Molyi", DisplayName = "AlertsLibrary", Description = "Unturned OpenMod Library. Used to send Alerts through the UI.", Website = "https://github.com/MolyiEZ/AlertsLibrary")]
 namespace Molyi.Alerts
 {
 	public class AlertsLibrary(
 		IServiceProvider serviceProvider) : OpenModUnturnedPlugin(serviceProvider)
     {
+		private readonly IServiceProvider m_ServiceProvider = serviceProvider;
+
 		protected override UniTask OnLoadAsync()
         {
-            return UniTask.CompletedTask;
+			m_ServiceProvider.GetRequiredService<IAlertsManager>();
+
+			return UniTask.CompletedTask;
         }
 
-        protected override UniTask OnUnloadAsync()
-        {
-			return UniTask.CompletedTask;
-		}
-    }
+        protected override UniTask OnUnloadAsync() => UniTask.CompletedTask;
+	}
 }
